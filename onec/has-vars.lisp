88abd89ffc144ff61904a3cd5c03c33c710a3416
ltmcmill <ltmcmill@ncsu.edu>
2019-02-17 22:43:48 -0500

    Got has-vars to work as far as I'm aware lol

---
+++ onec/has-vars.lisp
@@ -0,0 +1,12 @@
+(defun var? (x)
+  (and (symbolp x) 
+       (eql (char (symbol-name x) 0) #\?)))
+
+(defun has-vars (x)
+  (if (not x) (return-from has-vars nil))
+  (if (var? x) (return-from has-vars (cons x nil)))
+  (if (not (listp x)) (return-from has-vars nil))
+  (union (has-vars (car x))
+  (has-vars (cdr x))))
+
+(print (HAS-VARS '(AND (PARENT ?X ?Y) (MALE ?X))))
