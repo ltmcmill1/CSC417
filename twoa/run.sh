883c32222fb62116b64601d31d5769daefa1974a
Jack Schaffner <jackwschaffner@gmail.com>
2019-04-05 23:22:35 -0400

    Update message

--- twoa/run.sh
+++ twoa/run.sh
@@ -4,5 +4,7 @@ if [ ! -e install.true ]; then
     sudo bash install.sh
 else
     echo "Install already complete, starting ide..."
+    echo "Use: make <target>"
+    echo "to run the application"
     ./duo/etc/ide
 fi
